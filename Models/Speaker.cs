8a1d3bcfc4bc81d53b525af223c1a99c1a2bafa8
Marcus Toman <toman.markus12@gmail.com>
2023-06-05 16:35:42 +0300

    Added var sorted , made globalusings, changes to agendaviewmodels

---
+++ Models/Speaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConference.Models
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Image { get; set; } = "https://pbs.twimg.com/profile_images/1307714520870260739/lhweJTqL_400x400.jpg";
+    }
+}
---
+++ ViewModels/AgendaViewModels.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using MvvmHelpers;
+using MyConference.Models;
+using Jeffsum;
+using static Jeffsum.Goldblum;
+using UIKit;
+
+namespace MyConference.ViewModels;
+
+public partial class AgendaViewModel : ObservableObject
+{
+    public int Day { get; set; }
+    public ObservableRangeCollection<Grouping<string, Session>> Agenda { get; } = new();
+    Random random = new();
+    public AgendaViewModel()
+    {
+
+    }
+    [RelayCommand]
+    Task LoadDataAsync()
+    {
+        var sessioncount = new[] { 1, 2, 4, 4, 4, 4, 4, };
+        var sessions = new List<Session>();
+        var start = new DateTime(2022, 9, Day, 8, 30, 0);
+
+        for (int i = 0; i < sessioncount.Length; i++)
+            AddItems(sessioncount[i], i);
+
+        var sorted = from session in sessions
+                     orderby session.Start
+                     group session by session.StartTimeDisplay into sessionGroup
+                     select new Grouping<string, Session>(sessionGroup.Key, sessionGroup);
+
+        Agenda.AddRange(sorted);
+
+        return Task.CompletedTask;
+
+        void AddItems(int count, int offset)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                sessions.Add(new Session
+                {
+                    Title = string.Join(" ", ReceiveTheJeff(random.Next(2, 5), JeffsumType.Words)),
+                    Description = ReceiveTheJeff(1).First(),
+                    Room = "Goldblum",
+                    Start = start.AddHours(offset),
+                    End = start.AddHours(offset + 1)
+                });
+
+            }
+        }
+    }
